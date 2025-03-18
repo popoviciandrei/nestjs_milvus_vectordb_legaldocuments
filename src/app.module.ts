7f4e514b42091959ffcf45d5ab99c9bfa04f4496
Andrei Popovici <me@andreipopovici.co.uk>
2025-03-18 19:30:06 +0000

    Setup basic app start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { MilvusModule } from './milvus/milvus.module';
+import { LegalDocumentsModule } from './legal-documents/legal-documents.module';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, MilvusModule, LegalDocumentsModule, OpenaiModule],
   controllers: [AppController],
   providers: [AppService],
 })
